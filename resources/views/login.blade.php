49a3858c1b0d12ec959dc7ad948b48817d5d793c
Talha <ranatalhaa01@gmail.com>
2021-12-19 13:56:23 +0500

    login page front end

---
+++ resources/views/login.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sign In</title>
+    <link rel="stylesheet" href=" {{ url('css/main.css') }}">
+</head>
+
+<body>
+
+	<div class="login-box">
+
+		<h1>Login</h1>
+
+		<div class="textbox">
+			<input id="name" type="text" placeholder="Username" name="" value="">
+		</div>
+
+		<div class="textbox">
+			<input id="password" type="password" placeholder="Password" name="" value="">
+		</div>
+
+		<input id="logging" class="btn" type="button" name="" value="Sign in">
+	</div>
+
+	</div>
+
+	<script type="text/javascript">
+		document.getElementById("logging").onclick = function () {
+    var Username = document.getElementById("name");
+    var password = document.getElementById("password");
+
+    if (Username.value == "ADMIN" && password.value == "admin") {
+        window.location.href = "main.html";
+    }
+    else {
+        alert("INVALID Admin ID OR Password");
+    }
+}
+	</script>
+</body>
+
+</html>
+
+
+
